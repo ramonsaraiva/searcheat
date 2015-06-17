83b1efc8911d806bcc00afcbdb440d74e9487391
zeh <joseclaudio@maestroinformatica.com.br>
2015-06-17 22:10:40 +0000

    marker do map

--- client/controllers.js
+++ client/controllers.js
@@ -28,9 +28,28 @@ controllers.controller('city_controller', ['$scope', '$routeParams', '$location'
 controllers.controller('map_controller', ['$scope', function($scope) {
 	$scope.map = {
 		center: {
-			latitude: 45,
-			longitude: -73
+			latitude: 40.1451,
+			longitude: -99.6680
 		},
-		zoom: 8
+		zoom: 4
+	};
+
+	$scope.marker = {
+		id: 0,
+		coords: {
+			latitude: 40.1451,
+			longitude: -99.6680
+		},
+		options: {
+			draggable: true
+		},
+		events: {
+			dragend: function(marker, eventName, args) {
+				var lat = marker.getPosition().lat();
+				var lng = marker.getPosition().lng();
+				console.log(lat);
+				console.log(lng);
+			}
+		}
 	};
 }]);
