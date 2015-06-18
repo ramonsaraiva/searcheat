3801d69c629753a2f9e32b51652fdd3ab3727cce
zeh <joseclaudio@maestroinformatica.com.br>
2015-06-18 19:10:51 +0000

    markers funcionando

--- client/controllers.js
+++ client/controllers.js
@@ -29,47 +29,21 @@ controllers.controller('city_controller', ['$scope', '$routeParams', '$location'
 			latitude: 40.1451,
 			longitude: -99.6680
 		},
-		zoom: 8,
-		bounds: {}
+		zoom: 8
 	};
 
 	$scope.options = {
 		scrollwheel: false
 	};
 
-	var createRandomMarker = function(i, bounds, idKey) {
-		var lat_min = bounds.southwest.latitude,
-		lat_range = bounds.northeast.latitude - lat_min,
-		lng_min = bounds.southwest.longitude,
-			lng_range = bounds.northeast.longitude - lng_min;
-
-		if (idKey == null) {
-			idKey = "id";
+	$scope.trucks = [
+		{
+			id: 0,
+			latitude: 40.1451,
+			longitude: -99.6680,
+			title: 'igualop'
 		}
-
-		var latitude = lat_min + (Math.random() * lat_range);
-		var longitude = lng_min + (Math.random() * lng_range);
-		var ret = {
-			latitude: latitude,
-			longitude: longitude,
-			title: 'm' + i
-		};
-		ret[idKey] = i;
-		return ret;
-	};
-
-	$scope.randomMarkers = [];
-	$scope.$watch(function() {
-		return $scope.map.bounds;
-	}, function(nv, ov) {
-		if (!ov.southwest && nv.southwest) {
-			var markers = [];
-			for (var i = 0; i < 50; i++) {
-				markers.push(createRandomMarker(i, $scope.map.bounds))
-			}
-			$scope.randomMarkers = markers;
-		}
-	}, true);
+	];
 
 	/*
 	$scope.marker = {
