195ae83baf804f01a34be024e012fce17d0d4758
Zeh <joseclaudiovandame@gmail.com>
2015-06-18 14:41:30 -0300

    copy paste dum codigo de markers porque o meu zicou

--- client/controllers.js
+++ client/controllers.js
@@ -21,7 +21,7 @@ controllers.controller('guidance_controller', ['$scope', '$location', 'db', func
 }]);
 
 controllers.controller('city_controller', ['$scope', '$routeParams', '$location', 'db', function($scope, $routeParams, $location, db) {
-	$scope.db = new db('cities');
+	//$scope.db = new db('cities');
 	$scope.city = {}
 
 	$scope.map = {
@@ -29,9 +29,49 @@ controllers.controller('city_controller', ['$scope', '$routeParams', '$location'
 			latitude: 40.1451,
 			longitude: -99.6680
 		},
-		zoom: 8
+		zoom: 8,
+		bounds: {}
 	};
 
+	$scope.options = {
+		scrollwheel: false
+	};
+
+	var createRandomMarker = function(i, bounds, idKey) {
+		var lat_min = bounds.southwest.latitude,
+		lat_range = bounds.northeast.latitude - lat_min,
+		lng_min = bounds.southwest.longitude,
+			lng_range = bounds.northeast.longitude - lng_min;
+
+		if (idKey == null) {
+			idKey = "id";
+		}
+
+		var latitude = lat_min + (Math.random() * lat_range);
+		var longitude = lng_min + (Math.random() * lng_range);
+		var ret = {
+			latitude: latitude,
+			longitude: longitude,
+			title: 'm' + i
+		};
+		ret[idKey] = i;
+		return ret;
+	};
+
+	$scope.randomMarkers = [];
+	$scope.$watch(function() {
+		return $scope.map.bounds;
+	}, function(nv, ov) {
+		if (!ov.southwest && nv.southwest) {
+			var markers = [];
+			for (var i = 0; i < 50; i++) {
+				markers.push(createRandomMarker(i, $scope.map.bounds))
+			}
+			$scope.randomMarkers = markers;
+		}
+	}, true);
+
+	/*
 	$scope.marker = {
 		id: 0,
 		coords: {
@@ -58,4 +98,5 @@ controllers.controller('city_controller', ['$scope', '$routeParams', '$location'
 		.error(function(e) {
 			$scope.city = null;
 		});
+	*/
 }]);
